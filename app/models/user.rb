334fc9c24dafeb22d49164995c9cc7dfc05eb92b
KALi <aleksandr-slap@rambler.ru>
2021-07-21 17:07:11 +0300

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :tests
+  has_many :my_tests, class_name: "Test"
+
 	def result(level)
-	  Test.joins('JOIN user_tests ON tests.id = user_tests.test_id ').where(level: level).where('user_tests.user_id = ?', self.id)	
+	  Test.joins('JOIN tests_users ON tests.id = tests_users.test_id ').where(level: level).where('tests_users.user_id = ?', self.id)	
 	end	
 end
