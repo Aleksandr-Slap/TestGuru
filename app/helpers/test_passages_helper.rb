c30e4c39235d5791e5d12c2c94a717ae8cdbff7d
KALi <aleksandr-slap@rambler.ru>
2022-01-03 12:48:14 +0300

    Выполнен код из скринкаста и задание progress_bar

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module TestPassagesHelper
+  def progressbar_html
+    content_tag(:div,
+                content_tag(:div, nil,
+                            class: "progress-bar",
+                            'aria-valuemax' => 100,
+                            'aria-valuemin' => 0,
+                            'aria-valuenow' => 0,
+                            'role' => 'progressbar',
+                            'style' => "width: 0%",
+                            data: { questions_max: @test_passage.test.questions.length,
+                                    current_question_number: @test_passage.test.questions.find_index(@test_passage.current_question) }),
+                class: 'progress')
+  end
 end
