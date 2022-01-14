ef9346b61faaed5ff192a726c6c10a85ac0ed1be
KALi <aleksandr-slap@rambler.ru>
2022-01-14 16:40:07 +0300

    Новые изменения для успешного деплоя

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
+import * as bootstrap from "bootstrap";
 
 require("../utilities/sorting")
 require("../utilities/password_confirmation")
