db4c916d4ee85da37cf981021c2c0347fa33ecdf
KALi <aleksandr-slap@rambler.ru>
2022-01-24 16:15:40 +0300

    Added dependent: :destroy to models, fixed seeds.rb, moved footer

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
+  has_many :questions, dependent: :destroy
   has_many :users, through: :test_passages
-  has_many :questions
+
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,29 +2,23 @@
 
 Category.destroy_all
 
-categories = Category.create([{
-                               title: 'Frontend'
-                             },
+categories = Category.create([{ title: 'Frontend'},
                               { title: 'Backend' }])
 
 p "Created #{Category.count} categories"
 
 User.destroy_all
 
-users = User.create!([{
-                       name: 'Sasha',
-                       email: 'Sasha@gmail.com'
-                     },
-                      { name: 'Valera',
-                        email: 'Valera@gmail.com' }])
+users = User.create!([{ name: 'Sasha',
+                    password: 123456,
+       password_confirmation: 123456,
+                       email: 'aleksandrslap@gmail.com' }])
 
 p "Created #{User.count} users"
 
 Test.destroy_all
 
-tests = Test.create([{
-                      title: 'Ruby', level: 1, category_id: categories[1].id, user_id: users[0].id
-                    },
+tests = Test.create([{ title: 'Ruby', level: 1, category_id: categories[1].id, user_id: users[0].id },
                      { title: 'Rails', level: 2, category_id: categories[1].id, user_id: users[0].id },
                      { title: 'HTML', level: 3, category_id: categories[0].id, user_id: users[0].id }])
 
@@ -32,11 +26,9 @@ p "Created #{Test.count} tests"
 
 Question.destroy_all
 
-questions = Question.create([{
-                              body: 'What is HTML', test_id: tests[2].id
-                            },
-                             { body: 'Data types in Ruby', test_id: tests[0].id },
-                             { body: 'Rails ideas and principles', test_id: tests[1].id }])
+questions = Question.create([{ body: 'What is HTML?', test_id: tests[2].id },
+                             { body: 'Who created Ruby?', test_id: tests[0].id },
+                             { body: 'What language is Ruby on Rails written in?', test_id: tests[1].id }])
 
 p "Created #{Question.count} questions"
 
@@ -44,21 +36,41 @@ Answer.destroy_all
 
 answers = Answer.create([{
                           body: "Document markup language
-                        for web browsing in a browser",
-                          question_id: questions[0].id
-                        },
-                         { body: "Numbers.Boolean.Strings.Hashes.
-                         Arrays.Symbols.",
-                           question_id: questions[1].id },
-                         { body: "Optimization for the happiness of programmers
-                         Agreement is more important than configuration
-                         Omakase menu
-                         There is no single paradigm
-                         Excellent beautiful code
-                         Provide sharp knives
-                         Value integrated systems
-                         Progress is more important than stability
-                         Raise a big tent",
-                           question_id: questions[2].id }])
+                            for web browsing in a browser",
+                          question_id: questions[0].id,
+                          correct: true
+                         },
+                         { body: "Database management system",
+                           question_id: questions[0].id,
+                           correct: false
+                         },
+                         { body: "Matz",
+                           question_id: questions[1].id,
+                           correct: true
+                         },
+                         { body: "Bill Clinton",
+                           question_id: questions[1].id,
+                           correct: false
+                         },
+                         { body: "Tovarich Stalin",
+                           question_id: questions[1].id,
+                           correct: false
+                         },
+                         {body: "Mark Zuckerberg",
+                          question_id: questions[1].id,
+                          correct: false
+                         },
+                         { body: "C++",
+                           question_id: questions[2].id,
+                           correct: false
+                         },
+                         { body: "React",
+                           question_id: questions[2].id,
+                           correct: false
+                         },
+                         { body: "Ruby",
+                           question_id: questions[2].id,
+                           correct: true
+                         }])
 
 p "Created #{Answer.count} answers"
