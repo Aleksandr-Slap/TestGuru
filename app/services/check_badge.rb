c66dc5c721c2db21e71c04a938315993f9075504
KALi <aleksandr-slap@rambler.ru>
2022-05-11 19:32:45 +0300

    simplified the code, corrected the logic

--- app/services/check_badge.rb
+++ app/services/check_badge.rb
@@ -5,6 +5,7 @@ class CheckBadge
   def initialize(test_passage)
     @test_passage = test_passage
     @user = @test_passage.user
+    @test = @test_passage.test
   end 
 
   def call  
@@ -22,30 +23,32 @@ class CheckBadge
   end  
 
   def test_passage_first?(nothing)
-    TestPassage.where(user_id: @test_passage.user_id)
-    .where(test_id: @test_passage.test_id).count == 1 
+    number_of_passes = TestPassage.where(user_id: @user.id)
+                                  .where(test_id: @test.id).count
+    number_of_passes == 1                                   
   end
 
   def passed_all_category?(category)
-
     test_ids = Test.show_tests(category).ids
-    test_category_id = Test.find(@test_passage.test_id).category_id
-    
-    TestPassage.where(test_id: test_ids)
-       .where(user_id: @test_passage.user_id)
-       .where(passed: true)
-       .pluck(:test_id)
-       .uniq.count == test_ids.count && test_category_id == 25
+    test_category_id = @test.category.id
+    id_category = Category.find_by(title: category).id
+    passed_tests_of_one_category = TestPassage.where(test_id: test_ids)
+                                              .where(user_id: @user.id)
+                                              .where(passed: true)
+                                              .pluck(:test_id)
+                                              .uniq.count
+
+    passed_tests_of_one_category == test_ids.count && test_category_id == id_category
   end
 
   def passed_one_level_tests?(level)
-
-    test_ids = Test.where(level: level).ids
-    test_level_id = Test.find(@test_passage.test_id).level
-
-    TestPassage.where(test_id: test_ids)
-    .where(user_id: @test_passage.user_id).where(passed: true)
-    .pluck(:test_id)
-    .uniq.count == test_ids.count && test_level_id == level
+    test_ids = Test.where(level: level.to_i).ids
+    test_level_id = @test.level
+    passed_tests_of_the_same_level = TestPassage.where(test_id: test_ids)
+                                                .where(user_id: @user.id).where(passed: true)
+                                                .pluck(:test_id)
+                                                .uniq.count
+   
+    passed_tests_of_the_same_level == test_ids.count && test_level_id == level.to_i
   end
 end
